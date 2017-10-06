cb328f8bc97bc49863b699d3c4f8c7fafb3eacab
Matthew Lawton <mlawton@siue.edu>
2017-10-06 10:58:08 -0500

    Adding first working project

---
+++ src/main/java/com/ociweb/BuzzerBehavior.java
@@ -0,0 +1,32 @@
+package com.ociweb;
+
+import com.ociweb.gl.api.TimeListener;
+import com.ociweb.iot.maker.DigitalListener;
+import com.ociweb.iot.maker.FogCommandChannel;
+import com.ociweb.iot.maker.FogRuntime;
+import com.ociweb.iot.maker.Port;
+
+public class BuzzerBehavior implements TimeListener, DigitalListener {
+
+	private final FogCommandChannel buzzerChannel;
+	private final Port buzzerPort;
+	private final Port buttonPort;
+
+	private int buttonPressed = 0;
+
+	public BuzzerBehavior(FogRuntime runtime, Port buzzerPort, Port buttonPort) {
+		this.buzzerChannel = runtime.newCommandChannel(FogRuntime.PIN_WRITER);
+		this.buzzerPort = buzzerPort;
+		this.buttonPort = buttonPort;
+	}
+	@Override
+	public void digitalEvent(Port port, long time, long duration, int value) {
+		if (port == buttonPort) {
+			buttonPressed = value;
+		}
+	}
+	public void timeEvent(long time, int iteration) {
+		buzzerChannel.setValue(buzzerPort, buttonPressed);
+	}
+	
+}
---
+++ src/main/java/com/ociweb/IoTApp.java
@@ -0,0 +1,30 @@
+package com.ociweb;
+
+import static com.ociweb.iot.grove.simple_digital.SimpleDigitalTwig.Button;
+import static com.ociweb.iot.grove.simple_digital.SimpleDigitalTwig.Buzzer;
+import static com.ociweb.iot.maker.Port.D3;
+import static com.ociweb.iot.maker.Port.D4;
+
+import com.ociweb.iot.maker.FogApp;
+import com.ociweb.iot.maker.FogRuntime;
+import com.ociweb.iot.maker.Hardware;
+import com.ociweb.iot.maker.Port;
+
+
+public class IoTApp implements FogApp {
+           
+	public static Port BUZZER_PORT = D4;
+	public static Port BUTTON_PORT = D3;
+    
+    @Override
+    public void declareConnections(Hardware hardware) {
+        hardware.connect(Buzzer, BUZZER_PORT);
+        hardware.connect(Button, BUTTON_PORT);  
+        hardware.setTimerPulseRate(100);
+    }
+
+    @Override
+    public void declareBehavior(FogRuntime runtime) {
+    	runtime.registerListener(new BuzzerBehavior(runtime, BUZZER_PORT, BUTTON_PORT)); 
+    }  
+}
